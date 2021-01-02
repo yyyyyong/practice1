9c275dafe1f702d2916ae1db83590ecc115c6bbb
yyyyyong <yoonyh3519@gmail.com>
2021-01-02 19:45:24 +0900

    .

---
+++ src/main/java/com/yyh/book/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.yyh.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing // JPA auditing 활성화
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+       SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/test/java/com/yyh/book/springboot/domain/posts/PostsRepositoryTEST.java
@@ -0,0 +1,67 @@
+package com.yyh.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTEST {
+
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+
+        String title= "테스트게시글";
+        String content="테스트본문";
+
+        postsRepository.save(Posts.builder()
+            .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        LocalDateTime now = LocalDateTime.of(2021,1,2,18,50,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate"+posts.getCreatedDate()+",modifiedDate"+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+
+}
