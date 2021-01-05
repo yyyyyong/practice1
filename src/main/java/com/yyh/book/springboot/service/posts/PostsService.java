b78d9ae62f805d70cb1129807f11ec5a58c50e98
yyyyyong <yoonyh3519@gmail.com>
2021-01-05 10:42:18 +0900

    ..

--- src/main/java/com/yyh/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yyh/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.yyh.book.springboot.service.posts;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import com.yyh.book.springboot.domain.posts.Posts;
 import com.yyh.book.springboot.domain.posts.PostsRepository;
+import com.yyh.book.springboot.web.dto.PostsListResponseDto;
 import com.yyh.book.springboot.web.dto.PostsResponseDto;
 import com.yyh.book.springboot.web.dto.PostsSaveRequestDto;
 import com.yyh.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -31,5 +34,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다.id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
